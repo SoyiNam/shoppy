cbd640275c002c32437fb06257d0b5f230dbc290
Soyi <frontsoyi@gmail.com>
2024-06-11 01:20:32 +0900

    modify 경로

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,7 +6,7 @@ export default function Products() {
     <>
       <section></section>
       <section className="block w-full max-h-96 overflow-hidden">
-        <img src="../public/images/middle_banner.png" alt=""></img>
+        <img src="/images/middle_banner.png" alt=""></img>
       </section>
       <section>
         <ProductCard />
